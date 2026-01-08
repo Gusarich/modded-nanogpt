26238fed7d4e8b42ec71299bf90d25bdb636854d
Daniil Sedov <gusarich@icloud.com>
2026-01-09 02:19:32 +0300

    Reduce extension steps (25 instead of 40)

--- train_gpt.py
+++ train_gpt.py
@@ -1810,7 +1810,7 @@ class Hyperparameters:
     val_batch_size: int = 4 * 64 * 1024 * 8
     # optimization
     num_scheduled_iterations: int = 1735  # number of steps to complete lr and ws schedule
-    num_extension_iterations: int = 40  # number of steps to continue training at final lr and ws
+    num_extension_iterations: int = 25  # number of steps to continue training at final lr and ws
     num_iterations: int = num_scheduled_iterations + num_extension_iterations
     cooldown_frac: float = 0.50  # fraction of num_scheduled_iterations spent cooling down the learning rate
     split_embed_frac: float = 2/3  # fraction of training when embeddings split from lm_head
