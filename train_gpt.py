1a112decd3d55a3e9f428bac1c976579cb19bb28
Daniil Sedov <gusarich@icloud.com>
2026-01-09 03:56:19 +0300

    MLP: use matmul to avoid c_proj transpose

--- train_gpt.py
+++ train_gpt.py
@@ -1191,7 +1191,7 @@ class MLP(nn.Module):
     def forward(self, x: Tensor):
         x = F.linear(x, self.c_fc.type_as(x))
         x = F.relu(x).square() # https://arxiv.org/abs/2109.08668v2; ~1-2% better than GELU; suggested by @SKYLINEZ007 and @Grad62304977
-        x = F.linear(x, self.c_proj.T.type_as(x))
+        x = x @ self.c_proj.type_as(x)
         return x
 
 class Block(nn.Module):
